15162d17627b29a42c43edd0025fbfb23a2a9a4c
akashwebs <aksah.shil227@gmail.com>
2022-05-08 23:38:04 +0600

    added social login error messaage

--- src/Components/SocialLogin/SocialLogin.js
+++ src/Components/SocialLogin/SocialLogin.js
@@ -25,11 +25,22 @@ const SocialLogin = () => {
 
     const handleGitLogin = () => {
         signInWithGithub();
-    }
 
+    }
+   let errorMessage;
+    if(googleError){
+        errorMessage= <p>{googleError.message}</p>
+    } 
+    if(gitError){
+        errorMessage= <p>{gitError?.message}</p>
+    } 
+
+    
     return (
         <div className='text-center'>
             <h4>Another Login: </h4>
+            
+            <span className='text-danger d-block'>{googleError?.message} {gitError?.message}</span>
             <button onClick={handleGoogleLogin} className='btn btn-danger py-2 px-3'>Google</button> <br />
             <button onClick={handleGitLogin} className='btn bg-dark text-light py-2 px-3 my-2'>Github</button>
 
