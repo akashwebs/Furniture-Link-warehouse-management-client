bfe5c870d7fef68c901bc6fbdaf808ffee085f8f
akashwebs <aksah.shil227@gmail.com>
2022-05-08 12:37:24 +0600

    redesign footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaFacebookSquare,FaTwitterSquare,FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div className='mt-5 py-5 bg-dark text-light'>
-            <h2>this is footer</h2>
+        <div className='mt-5 py-5 bg-dark text-light text-center'>
+            <h1>Furniture Link</h1>
+            <div className='fs-5 d-grid g-3'>
+                <Link to={'/'}>home</Link>
+                <Link to={'/blog'}>blog</Link>
+                <Link to={'/'}>inventory</Link>
+            </div>
+            <div className='fs-2 '>
+                <Link className='text-white me-2' to={'/'}><FaFacebookSquare/></Link>
+                <Link className='text-white me-2' to={'/'}><FaTwitterSquare/></Link>
+                <Link className='text-white me-2' to={'/'}><FaLinkedin/></Link>
+            </div>
+        <div>
+            <p>Copyright &copy; {new Date().getFullYear()} Furniture Link All Rights Reserved</p> 
+        </div>
         </div>
     );
 };
--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -6,7 +6,8 @@ const NotFound = () => {
     
     return (
         <div className='container text-center my-5 py-5'>
-            <h2 style={{fontSize:'13rem',}} className='fw-bolder'>404</h2>
+            {/* <h2 style={{fontSize:'13rem',}} className='fw-bolder'>404</h2> */}
+            <img src={'https://templates.iqonic.design/posdash/html/assets/images/error/404.png '} className='img-fluid w-50' alt="" />
             <h3 className=''>opps! page not found</h3>
             <p>Sorry, the page your're looking for doesn't exist. If you think something is broken, report a problem.</p>
             <button onClick={()=>navigate('/')} className="btn-info py-2 px-3">Return Home</button>
